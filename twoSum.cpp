4611bc18aa18a20638bccbd43045cc740fbff0ff
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-02-13 16:29:35 +0800

    problem 1

---
+++ twoSum.cpp
@@ -0,0 +1,28 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+
+// Runtime: 4 ms, faster than 97.38% of C++ online submissions for Two Sum.
+// Memory Usage: 9 MB, less than 58.54% of C++ online submissions for Two Sum.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> cache;
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            
+            if(cache.find(need) == cache.end()) {
+                cache.insert(pair(nums[i], i));
+            }
+            else {
+                ans.push_back(cache[need]);
+                ans.push_back(i);
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
