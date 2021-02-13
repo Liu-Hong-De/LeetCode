3df6770d33f8dcf97e4aeb73130272d437015c66
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-02-13 17:52:08 +0800

    problem 7

---
+++ reverse.cpp
@@ -0,0 +1,21 @@
+/* Given a signed 32-bit integer x, return x with its digits reversed. 
+If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned). */
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Reverse Integer.
+// Memory Usage: 6 MB, less than 64.30% of C++ online submissions for Reverse Integer.
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x != 0) {
+            if(ans > INT_MAX / 10 || ans < INT_MIN / 10) {
+                return 0;
+            }
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        }
+        return ans;
+    }
+};
