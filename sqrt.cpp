6de3a3b2f27de2543eb6010ff13558b1cfcd9665
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-02-17 16:44:35 +0800

    problem 69

---
+++ sqrt.cpp
@@ -0,0 +1,37 @@
+/* Given a non-negative integer x, compute and return the square root of x.
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned. */
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Sqrt(x).
+// Memory Usage: 5.8 MB, less than 81.91% of C++ online submissions for Sqrt(x).
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int mid = x;
+        int high = x;
+        int low = 0;
+        
+        if(x == 0) {
+            return 0;
+        }
+        
+//        利用binary searcu的概念去逼近結果 
+        while(1) {
+            if(x / mid > mid) {
+                low = mid;
+                mid = low + (high-low) / 2;
+            }
+            else if(x / mid < mid) {
+                high = mid;
+                mid /= 2;
+            }
+            else {
+                return mid;
+            }
+            
+            if(high - low == 1) {
+                return low;
+            }
+        }
+    }
+};
