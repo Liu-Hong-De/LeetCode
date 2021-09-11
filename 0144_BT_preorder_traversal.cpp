0ca09f11d91d73658fdc2fe17b81e48b48374135
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-09-11 14:51:17 +0800

    problem 144

---
+++ 0144_BT_preorder_traversal.cpp
@@ -0,0 +1,34 @@
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Binary Tree Preorder Traversal.
+// Memory Usage: 8.5 MB, less than 45.94% of C++ online submissions for Binary Tree Preorder Traversal.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return ans;
+    }
+    
+    void preorder(TreeNode* root) {
+        if(root != NULL) {
+            ans.push_back(root->val);
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+    
+private:
+    vector<int> ans;
+};
