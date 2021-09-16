c19dc317fd8c68ce412f027d248fd3d069d2fa5d
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-09-16 16:51:12 +0800

    problem 169

---
+++ 0169_majority_element.cpp
@@ -0,0 +1,36 @@
+/* Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than floor(n / 2) times. 
+You may assume that the majority element always exists in the array. */
+
+// Runtime: 12 ms, faster than 96.48% of C++ online submissions for Majority Element.
+// Memory Usage: 19.6 MB, less than 51.88% of C++ online submissions for Majority Element.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+//    	統計出現次數 
+        for(int i = 0; i < nums.size(); i++) {
+            if(num.find(nums[i]) == num.end()) {
+                num[nums[i]] = 1;
+            }
+            else {
+                num[nums[i]] += 1;
+            }
+        }
+        
+//        找max 
+        for(auto i = num.begin(); i != num.end(); i++) {
+            if(i->second > count) {
+                count = i->second;
+                ans = i->first;
+            }
+        }
+        return ans;
+    }
+    
+private:
+    unordered_map<int, int> num;
+    int count = 0;
+    int ans;
+};
