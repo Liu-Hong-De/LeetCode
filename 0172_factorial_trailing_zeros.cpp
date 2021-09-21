6670897efcea086893208fc9447d335c94fc6e54
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-09-21 11:06:17 +0800

    problem 172

---
+++ 0172_factorial_trailing_zeros.cpp
@@ -0,0 +1,25 @@
+// Given an integer n, return the number of trailing zeroes in n!.
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Factorial Trailing Zeroes.
+// Memory Usage: 5.7 MB, less than 95.78% of C++ online submissions for Factorial Trailing Zeroes.
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+        while(n >= 5) {
+            if(n % 5 == 0) {
+                int temp = n;
+                while(temp % 5 == 0) {
+                    temp /= 5;
+                    count += 1;
+                }
+                n -= 5;
+            }
+            else {
+             n--;   
+            }
+        }
+        return count;
+    }
+};
