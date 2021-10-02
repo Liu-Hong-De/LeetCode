ddd0eb81a2859787997f58acd47fd17d3036a680
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-10-02 17:58:52 +0800

    problem 22

---
+++ 0024_swap_nodes_in_pairs.cpp
@@ -0,0 +1,30 @@
+/* Given a linked list, swap every two adjacent nodes and return its head. 
+You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.) */
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Swap Nodes in Pairs.
+// Memory Usage: 7.5 MB, less than 91.76% of C++ online submissions for Swap Nodes in Pairs.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) {
+            return head;
+        }
+        ListNode* node1 = head;
+        ListNode* node2 = head->next;
+        node1->next = node2->next;
+        node2->next = node1;
+        node1->next = swapPairs(node1->next);
+        return node2;
+    }
+};
