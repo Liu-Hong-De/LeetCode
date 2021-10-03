319fabda35bac4f4ac4229f6f462be8ba2ec09dd
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2021-10-03 10:11:55 +0800

    problem 55

---
+++ 0055_jump_game.cpp
@@ -0,0 +1,25 @@
+/* You are given an integer array nums. 
+You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise. */
+
+// Runtime: 56 ms, faster than 82.88% of C++ online submissions for Jump Game.
+// Memory Usage: 48.3 MB, less than 49.19% of C++ online submissions for Jump Game.
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int pos = 0;
+        int step = nums[0];
+        while(pos < nums.size()-1 && step != 0) {
+            pos++;
+            step = step-1 >nums[pos] ? step-1 : nums[pos];
+        }
+        if(pos == nums.size()-1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
