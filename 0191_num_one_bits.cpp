7431f93175afe3aa5875bd1aae1ac2d6fbc303d3
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:14:38 +0800

    Problem 0191 - Easy

--- 0191_num_one_bits.cpp
+++ 0191_num_one_bits.cpp
@@ -7,6 +7,7 @@ It should not affect your implementation, as the integer's internal binary repre
 In Java, the compiler represents the signed integers using 2's complement notation. 
 Therefore, in Example 3, the input represents the signed integer. -3. */
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of 1 Bits.
 // Memory Usage: 5.8 MB, less than 78.79% of C++ online submissions for Number of 1 Bits.
 
@@ -16,7 +17,7 @@ public:
         int ans = 0;
         uint32_t temp = n;
         
-//        ¥k²¾32bit 
+//        Â¥kÂ²Â¾32bit 
         for(int i = 0; i < 32; i++) {
             if(temp & 1 == 1) {
                 ans += 1;
