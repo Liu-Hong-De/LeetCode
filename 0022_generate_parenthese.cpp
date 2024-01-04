f52fc58c40b13358605ec59c4e05aa20a9835b43
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:00:13 +0800

    Problem 0022 - Medium

--- 0022_generate_parenthese.cpp
+++ 0022_generate_parenthese.cpp
@@ -1,5 +1,6 @@
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
+// Difficulty: Medium
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Generate Parentheses.
 // Memory Usage: 13.9 MB, less than 39.07% of C++ online submissions for Generate Parentheses.
 
