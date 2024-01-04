2f1de5c890e7e904132d0b65ebca95661cc19f94
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:16:05 +0800

    Problem 0258 - Easy

--- 0258_add_digits.cpp
+++ 0258_add_digits.cpp
@@ -1,5 +1,6 @@
 // Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Add Digits.
 // Memory Usage: 5.7 MB, less than 96.07% of C++ online submissions for Add Digits.
 
