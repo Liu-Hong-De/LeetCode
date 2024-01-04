a23ee7dbe11e768c59e63df6629e3cf1b1ef7c40
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:10:11 +0800

    Problem 0118 - Easy

--- 0118_Pascal_triangle.cpp
+++ 0118_Pascal_triangle.cpp
@@ -1,5 +1,6 @@
 // Given an integer numRows, return the first numRows of Pascal's triangle.
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Pascal's Triangle.
 // Memory Usage: 6.5 MB, less than 33.88% of C++ online submissions for Pascal's Triangle.
 
