a944d82873b1bdb2049824ebed8c73b07513f939
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:10:51 +0800

    Problem 0121 - Easy

--- 0121_best_time_to_buy_ans_sell_stock.cpp
+++ 0121_best_time_to_buy_ans_sell_stock.cpp
@@ -2,6 +2,7 @@
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
 
+// Difficulty: Easy
 // Runtime: 96 ms, faster than 98.10% of C++ online submissions for Best Time to Buy and Sell Stock.
 // Memory Usage: 93.3 MB, less than 91.08% of C++ online submissions for Best Time to Buy and Sell Stock.
 
