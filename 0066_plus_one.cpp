e57272074904f05254b2a5221b0a01aa9a1b1af8
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:05:56 +0800

    Problem 0066 - Easy

--- 0066_plus_one.cpp
+++ 0066_plus_one.cpp
@@ -2,6 +2,7 @@
 The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
 You may assume the integer does not contain any leading zero, except the number 0 itself. */
 
+// Difficulty: Easy
 // Runtime: 4 ms, faster than 51.43% of C++ online submissions for Plus One.
 // Memory Usage: 8.8 MB, less than 83.73% of C++ online submissions for Plus One.
 
