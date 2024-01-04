d653bfbecd32087c9fca3a65f3940eec1d19ccff
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:03:28 +0800

    Problem 0035 - Easy

--- 0035_search_insert_position.cpp
+++ 0035_search_insert_position.cpp
@@ -1,5 +1,6 @@
 /* Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. */
 
+// Difficulty: Easy
 // Runtime: 4 ms, faster than 90.05% of C++ online submissions for Search Insert Position.
 // Memory Usage: 9.6 MB, less than 92.04% of C++ online submissions for Search Insert Position.
 
