3e15aca6447f510573d0123fc6b1930b9f194809
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 11:56:46 +0800

    Problem 0009 - Easy

--- 0009_Palindrome Number.cpp
+++ 0009_Palindrome Number.cpp
@@ -1,6 +1,7 @@
 /* Given an integer x, return true if x is palindrome integer.
 An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not. */
 
+// Difficulty: Easy
 // Runtime: 4 ms, faster than 94.65% of C++ online submissions for Palindrome Number.
 // Memory Usage: 5.8 MB, less than 84.12% of C++ online submissions for Palindrome Number.
 
