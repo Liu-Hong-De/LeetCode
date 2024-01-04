48712eede0894db85a3defefb673e48a7d37342b
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:16:57 +0800

    Problem 0278 - Easy

--- 0278_find_bad_version.cpp
+++ 0278_find_bad_version.cpp
@@ -8,6 +8,7 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 You are given an API bool isBadVersion(version) which returns whether version is bad. 
 Implement a function to find the first bad version. You should minimize the number of calls to the API. */
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for First Bad Version.
 // Memory Usage: 5.8 MB, less than 89.70% of C++ online submissions for First Bad Version.
 
