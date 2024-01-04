2f2e9ba9cfcba805b272a6af773cb5a514c08873
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:13:36 +0800

    Problem 0169 - Easy

--- 0169_majority_element.cpp
+++ 0169_majority_element.cpp
@@ -3,13 +3,14 @@
 The majority element is the element that appears more than floor(n / 2) times. 
 You may assume that the majority element always exists in the array. */
 
+// Difficulty: Easy
 // Runtime: 12 ms, faster than 96.48% of C++ online submissions for Majority Element.
 // Memory Usage: 19.6 MB, less than 51.88% of C++ online submissions for Majority Element.
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-//    	²Î­p¥X²{¦¸¼Æ 
+//    	Â²ÃŽÂ­pÂ¥XÂ²{Â¦Â¸Â¼Ã† 
         for(int i = 0; i < nums.size(); i++) {
             if(num.find(nums[i]) == num.end()) {
                 num[nums[i]] = 1;
@@ -19,7 +20,7 @@ public:
             }
         }
         
-//        §ämax 
+//        Â§Ã¤max 
         for(auto i = num.begin(); i != num.end(); i++) {
             if(i->second > count) {
                 count = i->second;
