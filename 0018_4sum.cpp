19cd5d96ba9063d967ca1ebdcfe3697febc0730f
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 11:59:44 +0800

    Problem 0018 - Medium

--- 0018_4sum.cpp
+++ 0018_4sum.cpp
@@ -4,13 +4,14 @@
 	nums[a] + nums[b] + nums[c] + nums[d] == target
 You may return the answer in any order. */
 
+// Difficulty: Medium
 // Runtime: 68 ms, faster than 58.00% of C++ online submissions for 4Sum.
 // Memory Usage: 13.2 MB, less than 42.67% of C++ online submissions for 4Sum.
 
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-//    	nums¤¤­Èªº¼Æ¶q¤p©ók 
+//    	numsä¸­å€¼çš„æ•¸é‡å°æ–¼k 
         if(nums.size() < 4) {
             return ans;
         }
@@ -19,7 +20,7 @@ public:
         
         for(int i = 0; i < nums.size()-4+1; i++) {
             for(int j = i+1; j < nums.size()-4+2; j++) {
-//            	¥Ø«e©Ò»İ­ÈÁ`©M 
+//            	ç›®å‰æ‰€éœ€å€¼ç¸½å’Œ 
                 int need = target - nums[i] - nums[j];
                 int left = j + 1, right = nums.size() - 1;
                 
@@ -31,7 +32,7 @@ public:
                         temp.push_back(nums[right]);
                         ans.push_back(temp);
                         
-//                        ¬Û¦Pªº­È¶·¸õ¹L 
+//                        ç›¸åŒçš„å€¼é ˆè·³é 
                         while(left < right && nums[left] == temp[2]) {
                             left++;
                         }
@@ -40,11 +41,11 @@ public:
                         }
                         temp.clear();
                     }
-//                    ¥Ø«e­È¸û»İ¨D¤p 
+//                    ç›®å‰å€¼è¼ƒéœ€æ±‚å° 
                     else if(need > nums[left] + nums[right]) {
                         left++;
                     }
-//                    ¥Ø«e­È¸û»İ¨D¤j 
+//                    ç›®å‰å€¼è¼ƒéœ€æ±‚å¤§ 
                     else {
                         right--;
                     }
