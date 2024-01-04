fe8277cc04e2ef6296b29f07f77738e4d6e8c291
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:13:54 +0800

    Problem 0171 - Easy

--- 0171_excel_column_num.cpp
+++ 0171_excel_column_num.cpp
@@ -1,5 +1,6 @@
 /* Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number. */
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Excel Sheet Column Number.
 // Memory Usage: 6.3 MB, less than 10.97% of C++ online submissions for Excel Sheet Column Number.
 
