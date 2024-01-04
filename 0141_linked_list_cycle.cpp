db21143ccea7d237f1af6dff5868a375e8585e75
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:11:57 +0800

    Problem 0141 - Easy

--- 0141_linked_list_cycle.cpp
+++ 0141_linked_list_cycle.cpp
@@ -3,6 +3,7 @@ There is a cycle in a linked list if there is some node in the list that can be 
 Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 Return true if there is a cycle in the linked list. Otherwise, return false. */ 
 
+// Difficulty: Easy
 // Runtime: 10 ms, faster than 74.37% of C++ online submissions for Linked List Cycle.
 // Memory Usage: 8.1 MB, less than 58.56% of C++ online submissions for Linked List Cycle
 
