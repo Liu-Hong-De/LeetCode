4b3d7a5560afbd360113cbb66f3036e54e18e8ee
Liu-Hong-De <doggyhongdeliu@gmail.com>
2024-01-04 14:02:12 +0800

    Problem 0028 - Easy

--- 0028_implement_strStr.cpp
+++ 0028_implement_strStr.cpp
@@ -1,6 +1,7 @@
 /* Implement strStr().
 Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. */
 
+// Difficulty: Easy
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Implement strStr().
 // Memory Usage: 6.7 MB, less than 80.69% of C++ online submissions for Implement strStr().
 
